e734c2b9fab2494cf40a1a500bc696b9de1b7720
Stanley Strawbridge <stanley.strawbridge@gmail.com>
2024-12-03 13:32:48 +0000

    added chinnaiya folder

---
+++ chinnaiya_et_al_2024/main.m
@@ -0,0 +1,293 @@
+
+% USER INPUTS ============================================================
+
+image_type = 'sum';
+
+projection_method = 'linear';
+
+data_path = fullfile(pwd,'data');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Add paths ===============================================================
+
+src_path = fullfile(pwd,'src');
+addpath(src_path)
+
+load(fullfile(data_path,'segmentation'))
+
+% Use raw sum stacks for quantification, not max_..._rgb
+image_files = gTruth.DataSource.Source;
+
+image_files = erase(image_files,'-1');
+image_files = strrep(image_files,'e_max_projections_rgbs','c_sum_stacks');
+image_files = strrep(image_files,'MAX_','SUM_');
+
+figure_save_path = fullfile(data_path,'projections',...
+    image_type,'figures','raw');
+obj_save_path = fullfile(data_path,'projections',image_type,'objects');
+data_save_path = fullfile(data_path,'projections',image_type,'data');
+
+if ~exist(figure_save_path,'dir')
+    mkdir(figure_save_path);
+end
+
+if ~exist(obj_save_path,'dir')
+    mkdir(obj_save_path);
+end
+
+if ~exist(data_save_path,'dir')
+    mkdir(data_save_path);
+end
+
+% Step through images------------------------------------------------------
+
+for i = 1:numel(image_files)
+    
+    disp(['Data set ',num2str(i),' of ', num2str(numel(image_files))])
+    
+    % Make image matrix----------------------------------------------------
+    imageInfo = imfinfo(image_files{i});
+    pixel_size = 1/imageInfo(1).XResolution; %(um/px)
+    n_channels = numel(imageInfo);
+    im = zeros(imageInfo(1).Height,imageInfo(1).Width,n_channels);
+    
+    for j = 1:n_channels
+        im(:,:,j) = imread(image_files{i},j);
+    end
+
+    % make roi mask--------------------------------------------------------    
+    hull_points =  gTruth.LabelData.roi{i};
+    image_size = [imageInfo(1).Height,imageInfo(1).Width];
+    
+    [roi_mask, roi_mask_pixels] = makeRoiMask(hull_points,image_size);
+    
+    % make intensity matrix------------------------------------------------
+    idx = sub2ind(size(im(:,:,1)),...
+                       roi_mask_pixels(:,2),...
+                       roi_mask_pixels(:,1));
+    
+    intensity = zeros(numel(idx),n_channels);
+    
+    for j = 1:n_channels
+        x = im(:,:,j);
+        intensity(:,j) = x(idx);
+    end
+
+
+    % make manifolds-------------------------------------------------------
+    
+    manifold = gTruth.LabelData.(field_names{2}){:};
+
+    roi_mask_pixels = roi_mask_pixels*pixel_size;
+    manifold = manifold{:}*pixel_size;
+    
+    obj = projectMyPixel(roi_mask_pixels,intensity,manifold,...
+        projection_method,pixel_size);
+       
+    % flip AP
+    obj.projection_absolute = ...
+        abs(max(obj.projection_absolute)-obj.projection_absolute);
+    obj.projection_fractional = ...
+        abs(max(obj.projection_fractional)-obj.projection_fractional);
+
+    % Save data and objects -----------------------------------------------
+    image_name = extractBetween(image_files{i},'\SUM_','.');
+    image_name = image_name{:};
+
+    save_name = fullfile(obj_save_path,['obj_',image_name,'.mat']);
+    save(save_name, 'obj')
+
+    csv_save_name = fullfile(data_save_path,['data_',image_name,'.txt']);
+    writeData2csv(obj, csv_save_name)
+
+    % Make figures --------------------------------------------------------
+    stage = extractBetween(image_name,'stage=','_');
+    sample = extractBetween(image_name,'sample=','_');
+    projection_name = extractBetween(image_name,'projection=','_');
+    channel_names = strcat('c0',[extractAfter(image_name,'_c0')]);
+    
+    marker_names = split(channel_names,'_');
+
+    marker_colors = [0.0 0.0 0.7;
+                     0.0 0.7 0.0;
+                     0.7 0.0 0.0];
+
+    save_folder = string(fullfile(figure_save_path,...
+        extractBefore(image_name,'_projection')));
+
+    if ~exist(save_folder,'dir')
+        mkdir(save_folder);
+    end
+
+    % PLOT RAW DATA -------------------------------------------------------
+
+    plotRawFluorescence(obj,save_folder,marker_names);
+
+    % PLOT PROJECTIONS ----------------------------------------------------
+
+    plotData(obj, projection_name,marker_names,marker_colors,save_folder)
+
+end
+
+
+% percentile traces
+trace_save_path = fullfile(data_path,'raw_percentile_traces',image_type);
+
+if ~exist(trace_save_path,'dir')
+    mkdir(trace_save_path);
+end
+
+obj_files = dir(fullfile(obj_save_path,'*.mat'));
+
+for i = 1:numel(obj_files)
+    
+    load(fullfile(obj_files(i).folder,obj_files(i).name))
+    
+    traces = percentileTraces(obj,obj_files(i),trace_save_path);    
+    
+end
+
+
+%% Static functions========================================================
+function writeData2csv(obj, save_name)
+
+    data = [obj.coordinates, obj.projection_absolute,...
+        obj.projection_fractional, obj.intensity];
+    
+    writematrix(data,save_name,'Delimiter',",")
+
+ 
+end
+
+
+function [mask, mask_pixels] = makeRoiMask(hull_points,image_size)%--------
+
+    % Make a hull around the segmented ROI
+    shp = alphaShape(hull_points,inf);
+        
+    % Make pixel coordinate pairs 
+    [idx, jdx] = meshgrid([1:image_size(2)],[1:image_size(1)]);
+    pixel_coordinates = [idx(:), jdx(:)];
+
+    % get the pixels within the hull
+    mask_pixel_idx = shp.inShape(pixel_coordinates);
+    
+    % make mask matrix
+    mask_pixels= pixel_coordinates(mask_pixel_idx,:);
+    
+    mask = reshape(mask_pixel_idx,image_size(1),image_size(2))==1;
+        
+end%-----------------------------------------------------------------------
+
+function f = plotRawFluorescence(obj,save_folder,marker_names )        
+
+
+    f = obj.intensity;
+    f_logged = log10(f+1);     
+    
+    x = obj.coordinates(:,1);
+    y = obj.coordinates(:,2);
+    
+    % ROI with signal -----------------------------------------------------
+    
+    fig_1 = figure(1);
+    
+    for i = 1:size(f_logged,2)
+        
+        clf
+        
+        scatter3(x,y,f(:,i),1,f_logged(:,i))
+        view(2)
+        
+        caxis([min(f_logged(:,i)), max(f_logged(:,i))])
+        
+        colorbar
+
+        title(marker_names{i})
+
+        axis equal
+        axis ij
+
+        save_name = ['roi_' marker_names{i}];
+        saveFigure(fig_1,save_folder,save_name)
+                
+    end
+
+    close(fig_1)
+
+    % Fluorescence distribution -------------------------------------------
+
+    fig_1 = figure(1);
+    clf
+    fig_1.Position = [0   0  1000  1000];
+    plotmatrix(f)
+    axis equal
+    title({'Raw Fluorescence',...
+        strjoin([strcat(marker_names(1:end-1)',','),marker_names(end)])})
+
+    save_name = 'fluorescence_raw';
+    saveFigure(fig_1,save_folder,save_name)
+    close(fig_1)
+
+    fig_2 = figure(2);
+    clf
+    fig_2.Position = [0   0  1000  1000];
+    plotmatrix(f_logged)
+    axis equal
+    title({'log10(Raw Fluorescence + 1)',...
+        strjoin([strcat(marker_names(1:end-1)',','),marker_names(end)])})
+
+    save_name = 'fluorescence_logged';
+    saveFigure(fig_2,save_folder,save_name)
+    close(fig_2)
+
+end
+
+function plotData(obj,projection_name,marker_names,marker_colors,save_path)
+ 
+    fluorescence = log10(obj.intensity+1);
+    
+    projection_type = {'projection_absolute', 'projection_fractional'};
+
+    for i = 1:2
+
+        fig = figure(1);
+        fig.Position = [0   0  1000  1000];
+        clf
+        hold on   
+
+        for j = 1:size(fluorescence,2)    
+        
+            subplot(size(fluorescence,2)  ,1,j)
+
+            scatter(obj.(projection_type{i}),fluorescence(:,j),3,...
+                    'MarkerFaceColor',marker_colors(j,:),...
+                    'MarkerFaceAlpha',0.1, 'MarkerEdgeColor','none')                       
+            
+            title(marker_names{j}) 
+
+            axis tight
+            ylim([min(fluorescence(:,j)), max(fluorescence(:,j))])
+            
+
+        end
+
+        save_name = strcat(projection_name, '_', projection_type{i});
+        saveFigure(fig,save_path,save_name)      
+
+    end
+
+    close(fig)
+
+end
+
+function saveFigure(fig,save_folder,save_name)
+            
+            fullSaveName = fullfile(save_folder,save_name);
+             
+            savefig(fig,strcat(fullSaveName,'.fig'))
+            saveas(fig,strcat(fullSaveName,'.png'),'png')
+            saveas(fig,strcat(fullSaveName,'.svg'),'svg')
+            
+end
---
+++ chinnaiya_et_al_2024/src/percentileTraces.m
@@ -0,0 +1,369 @@
+classdef percentileTraces
+
+
+    properties %===========================================================
+        projection
+        file
+        save_path
+
+        n_bins = 500
+        centiles = [0 10 25 50 75 90]
+        bins_absolute
+        bins_relative
+        values
+
+        % Sampel Metadata
+        stage
+        sample_number
+        marker_names
+        number_of_markers
+
+
+
+    end %==================================================================
+
+
+    methods %==============================================================
+
+
+        function traces = percentileTraces(projection,file,save_path) %---- 
+
+            % initializ inputs
+            traces.projection = projection;
+            traces.file = file;
+            
+
+            traces = setIntput(traces,save_path);
+
+            traces = traces.makeTraces;
+
+            traces.plotTraces;
+
+            traces.overlayTraces;
+
+            traces.overlayTracesWithIQR;
+
+            save(traces.save_path,'traces')
+
+        end %--------------------------------------------------------------
+
+        
+        function traces = setIntput(traces,save_path) %--------------------
+
+            % Sampel Metadata
+            metadata = traces.file.name;
+
+            traces.stage = extractBetween(metadata,'stage=','_');
+            traces.sample_number = extractBetween(metadata,'sample=','_');
+ 
+            folder = extractBetween(metadata,'stage=','_projection');
+            traces.save_path = fullfile(save_path,folder{:});
+            if ~exist(traces.save_path,'dir')
+                mkdir(traces.save_path);
+            end
+            
+            channel_names = strcat('c0',[extractAfter(metadata,'_c0')]);
+            traces.marker_names = split(channel_names,'_');
+            traces.number_of_markers = numel(traces.marker_names);
+
+            if size(traces.projection.intensity,1) < traces.n_bins
+                traces.n_bins = size(traces.projection.intensity,2);
+            end
+
+            traces.values = zeros(traces.n_bins,...
+                                  numel(traces.centiles),...
+                                  traces.number_of_markers);
+
+        end %--------------------------------------------------------------
+
+
+        function traces = makeTraces(traces) %-----------------------------
+            
+            [min_d,max_d] = bounds(traces.projection.projection_absolute);
+            
+            bins = linspace(min_d,max_d+1,traces.n_bins+1)';
+
+            traces.bins_absolute = bins(1:end-1);
+            traces.bins_relative =  traces.bins_absolute - min(traces.bins_absolute);
+            traces.bins_relative =  traces.bins_absolute./max(traces.bins_absolute);
+
+            for i = 1:traces.n_bins
+
+                for j = 1:traces.number_of_markers
+                
+                    start = bins(i);
+                    stop =  bins(i+1);  
+                    
+                    idx = traces.projection.projection_absolute >=start & ...
+                    traces.projection.projection_absolute < stop;
+                        
+                    traces.values(i,:,j) = prctile(traces.projection.intensity(idx,j),traces.centiles);
+    
+                end
+
+            end
+
+        end %--------------------------------------------------------------
+
+
+        function plotTraces(traces) %--------------------------------------
+
+            smoothing = 20;
+
+            n_centiles = numel(traces.centiles);
+            c = [linspace(0,0.3922,n_centiles); 
+                linspace(0,0.5843,n_centiles);
+                linspace(0,0.9294,n_centiles)]';
+
+            line_widths = [2.5*ones(1,3), 5, 2.5*ones(1,2)];
+
+            for i = 1:traces.number_of_markers
+
+                fig1 = figure(1);clf;hold on;
+                fig2 = figure(2);clf;hold on;
+
+                for j = 1:n_centiles
+
+                    figure(1)
+                    plot(traces.bins_absolute, smooth(traces.values(:,j,i),...
+                        smoothing),LineWidth=line_widths(j),Color=c(j,:))
+                    
+                    figure(2)
+                    plot(traces.bins_relative, smooth(traces.values(:,j,i),...
+                        smoothing),LineWidth=line_widths(j),Color=c(j,:))
+
+                end
+
+                if  isempty(extractBefore(traces.marker_names{i},'.'))
+                    marker = traces.marker_names{i};
+                else
+                    marker = extractBefore(traces.marker_names{i},'.');
+                end                          
+
+                title(strcat(traces.stage{:},'/',traces.sample_number ,'/',marker))
+
+                figure(1)
+
+                legend(['min',string(traces.centiles(2:end))],...
+                    'Location','southoutside','Orientation','horizontal')
+                xlabel('Distance (\mum)')
+                ylabel('Fluorescence')                
+                title(strcat(traces.stage{:},'/',traces.sample_number ,'/',marker))
+                set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+
+                axis tight
+                
+                save_name = [traces.marker_names{i}, '_absolute_distance'];
+                traces.saveFigure(fig1,save_name);
+
+                figure(2)
+                legend(['min',string(traces.centiles(2:end))],...
+                    'Location','southoutside','Orientation','horizontal')
+                xlabel('Distance')
+                ylabel('Fluorescence')
+
+                set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+                
+                axis tight
+                
+                save_name = [traces.marker_names{i}, '_relative_distance'];             
+                traces.saveFigure(fig2,save_name);
+
+            end
+
+        end %--------------------------------------------------------------
+       
+        function overlayTraces(traces) %--------------------------------------
+            
+            smoothing = 20;
+
+            x = permute(traces.values(:,4,:), [1 3 2]);
+            
+            for i = 1:traces.number_of_markers
+                x(:,i) = smooth(x(:,i),smoothing);
+            end
+
+            x = x - repmat(min(x),size(x,1),1);
+            x = x ./ repmat(max(x),size(x,1),1);
+            
+            fig1 = figure(1);
+            clf
+            hold on
+
+            fig2 = figure(2);
+            clf
+            hold on
+
+            c = {'g','r','b'};
+            labels = {};
+
+            for i = 2:traces.number_of_markers
+                
+                figure(1);
+
+                plot(traces.bins_relative,x(:,i),c{i-1},'LineWidth',3)
+                shg
+
+                axis([0 max(traces.bins_relative) 0 1])
+
+                figure(2);
+
+                plot(traces.bins_absolute,x(:,i),c{i-1},'LineWidth',3)
+
+                axis([0 max(traces.bins_absolute) 0 1])
+
+                shg
+                          
+                if  isempty(extractBefore(traces.marker_names{i},'.'))
+                    labels = [labels;traces.marker_names{i}];
+                else
+                    label = {extractBefore(traces.marker_names{i},'.')};
+                    labels = [labels;label];
+                end
+
+            end
+
+            figure(1);
+            grid on
+            xlabel('Distance')
+            ylabel('Fluorescence')
+            title(strcat(traces.stage{:},'/',traces.sample_number))
+            set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+            legend(labels,'Location','southoutside',Orientation='horizontal' )
+            
+            figure(2);
+            grid on
+            xlabel('Distance  (\mum)')
+            ylabel('Fluorescence')
+            title(strcat(traces.stage{:},'/',traces.sample_number))
+            set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+            legend(labels,'Location','southoutside',Orientation='horizontal' )
+
+            save_name = 'overlay_relative_distance';             
+            traces.saveFigure(fig1,save_name);
+                        
+            save_name = 'overlay_absolute_distance';             
+            traces.saveFigure(fig2,save_name);
+
+        end %--------------------------------------------------------------
+
+
+        function overlayTracesWithIQR(traces) %--------------------------------------
+            
+            smoothing = 20;
+            
+            x_25 = permute(traces.values(:,3,:), [1 3 2]);
+            x_50 = permute(traces.values(:,4,:), [1 3 2]);
+            x_75 = permute(traces.values(:,5,:), [1 3 2]);
+            
+            for i = 1:traces.number_of_markers
+
+                x_25(:,i) = smooth(x_25(:,i),smoothing);
+                x_50(:,i) = smooth(x_50(:,i),smoothing);
+                x_75(:,i) = smooth(x_75(:,i),smoothing);
+
+            end
+
+            x_25 = x_25 - repmat(min(x_50),size(x_25,1),1);
+            x_75 = x_75 - repmat(min(x_50),size(x_75,1),1);
+            x_50 = x_50 - repmat(min(x_50),size(x_50,1),1);
+
+    
+            x_25 = x_25 ./ repmat(max(x_50),size(x_25,1),1);
+            x_75 = x_75 ./ repmat(max(x_50),size(x_75,1),1);
+            x_50 = x_50 ./ repmat(max(x_50),size(x_50,1),1);
+                   
+            fig1 = figure(1);
+            clf
+            hold on
+
+            fig2 = figure(2);
+            clf
+            hold on
+
+            c = {'g','r','b'};
+            labels = {};
+
+            for i = 2:traces.number_of_markers
+                
+                figure(1);
+                hold on
+                x_grid = [traces.bins_relative; flip(traces.bins_relative)];
+                y = [x_25(:,i); flip(x_75(:,i))];
+                
+                fill(x_grid,y,c{i-1},'EdgeColor','none','FaceAlpha',0.2)
+
+                plot(traces.bins_relative,x_50(:,i),c{i-1},'LineWidth',5)
+
+                axis([0 max(traces.bins_relative) 0 1])
+
+                shg
+
+                figure(2);
+                hold on
+                x_grid = [traces.bins_absolute; flip(traces.bins_absolute)];
+                y = [x_25(:,i); flip(x_75(:,i))];
+                
+                fill(x_grid,y,c{i-1},'EdgeColor','none','FaceAlpha',0.2)
+
+                plot(traces.bins_absolute,x_50(:,i),c{i-1},'LineWidth',5)
+
+                axis([0 max(traces.bins_absolute) 0 1])
+
+                shg
+                          
+                if  isempty(extractBefore(traces.marker_names{i},'.'))
+                    labels = [labels;traces.marker_names{i};traces.marker_names{i}];
+                else
+                    label = {extractBefore(traces.marker_names{i},'.')};
+                    labels = [labels;label; label];
+                end
+
+            end
+
+            figure(1);
+            grid on
+            xlabel('Distance')
+            ylabel('Fluorescence')
+            ylim([0 1])
+            title(strcat(traces.stage{:},'/',traces.sample_number))
+            set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+            legend(labels,'Location','southoutside',Orientation='horizontal' )
+            set(gcf,'Position' ,[0 0 950 950])
+
+
+            figure(2);
+            grid on
+            xlabel('Distance  (\mum)')
+            ylabel('Fluorescence')
+            ylim([0 1])
+            title(strcat(traces.stage{:},'/',traces.sample_number))
+            set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold')
+            legend(labels,'Location','southoutside',Orientation='horizontal' )
+            set(gcf,'Position' ,[0 0 950 950])
+
+            save_name = 'overlayIQR_relative_distance';             
+            traces.saveFigure(fig1,save_name);
+            close(fig1)
+            
+            save_name = 'overlayIQR_absolute_distance';             
+            traces.saveFigure(fig2,save_name);
+            close(fig2)
+            
+        end %--------------------------------------------------------------
+
+
+        function saveFigure(traces,fig,save_name) %-----------------------
+            
+            full_save_name = fullfile(traces.save_path,save_name);
+                        
+            savefig(fig,strcat(full_save_name,'.fig'))
+            saveas(fig,strcat(full_save_name,'.png'),'png')
+            saveas(fig,strcat(full_save_name,'.svg'),'svg')
+
+        end %--------------------------------------------------------------
+
+
+    end %==================================================================
+   
+end
